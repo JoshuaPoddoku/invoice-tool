2446c3bc89056efe1357efa72582839e851b4282
JoshuaPoddoku <joshuapoddoku@gmail.com>
2021-08-06 08:33:40 +0530

    resolved next module error

--- app-frontend/components/Invoice/Invoice.js
+++ app-frontend/components/Invoice/Invoice.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import Image from 'next/Image'
+import Image from 'next/image'
 import Pdf from 'react-to-pdf'
 import styles from './Invoice.module.scss'
 
